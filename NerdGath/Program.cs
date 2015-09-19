9cd31e17a1df0f8c38db89192f34449e1fbd0ab7
berbb <tintinridge@gmail.com>
2015-09-19 15:19:37 -0500

    NerdGath 1.0

---
+++ NerdGath/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace NerdGath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static Spell.Skillshot Q;
+        public static Spell.Skillshot W;
+        public static Spell.Active E;
+        public static Spell.Targeted R;
+
+        private static Slider manaC;
+        public static int MinNumberManaC { get { return manaC.CurrentValue; } }
+
+        private static Slider manaH;
+        public static int MinNumberManaH { get { return manaH.CurrentValue; } }
+
+        public static Menu GathMenu, ComboMenu, HarassMenu, FleeMenu, MiscMenu, FarmMenu, KSMenu;
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            TargetSelector2.init();
+            Bootstrap.Init(null);
+
+            Q = new Spell.Skillshot(SpellSlot.Q, 940, SkillShotType.Circular);
+            W = new Spell.Skillshot(SpellSlot.W, 290, SkillShotType.Cone);
+            E = new Spell.Active(SpellSlot.E);
+            R = new Spell.Targeted(SpellSlot.R, 175);
+
+            GathMenu = MainMenu.AddMenu("NerdGath", "NerdGath");
+            GathMenu.AddGroupLabel("NerdGath");
+            GathMenu.AddSeparator();
+            GathMenu.AddLabel("Nerd Series - Downloading More Ram");
+            GathMenu.AddLabel("Berb @ EloBuddy");
+            GathMenu.AddSeparator();
+            GathMenu.AddLabel("TO-DO ::");
+
+            ComboMenu = GathMenu.AddSubMenu("Combo", "Combo");
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("useQCombo", new CheckBox("Use Q"));
+            ComboMenu.Add("useWCombo", new CheckBox("Use W"));
+            ComboMenu.Add("useRCombo", new CheckBox("Use R"));
+            ComboMenu.Add("useRKill", new CheckBox("Ult only if killable (-Use R- MUST be enabled!)"));
+            ComboMenu.Add("igniteKill", new CheckBox("Ignite if Killable"));
+            ComboMenu.AddSeparator();
+            manaC = ComboMenu.Add("manamanager", new Slider("Minimum mana to do combo (%)", 20, 1, 100));
+
+            HarassMenu = GathMenu.AddSubMenu("Harass", "Harass");
+            HarassMenu.AddGroupLabel("Harass Settings");
+            HarassMenu.AddSeparator();
+            HarassMenu.Add("useQHarass", new CheckBox("Use Q"));
+            HarassMenu.Add("useWHarass", new CheckBox("Use W"));
+            HarassMenu.AddSeparator();
+            manaH = HarassMenu.Add("manamanager", new Slider("Minimum mana to harass (%)", 20, 1, 100));
+
+            FleeMenu = GathMenu.AddSubMenu("Flee", "Flee");
+            FleeMenu.AddGroupLabel("Flee Settings");
+            FleeMenu.AddSeparator();
+            FleeMenu.Add("useQFlee", new CheckBox("Use Q"));
+
+            KSMenu = GathMenu.AddSubMenu("KSMenu", "KSMenu");
+            KSMenu.AddGroupLabel("Kill Steal Settings");
+            KSMenu.AddSeparator();
+            KSMenu.Add("doKS", new CheckBox("Enable KS"));
+            KSMenu.Add("qKS", new CheckBox("Use Q to KS"));
+            KSMenu.Add("wKS", new CheckBox("Use W to KS"));
+            KSMenu.Add("rKS", new CheckBox("Use R to KS"));
+            KSMenu.Add("igniteKill", new CheckBox("Use Ignite to KS"));
+
+            FarmMenu = GathMenu.AddSubMenu("Farm", "Farm");
+            FarmMenu.AddGroupLabel("Farming Settings");
+            FarmMenu.AddLabel("NOTE : The R/Ult usage on farming is only if the mob is killable");
+            FarmMenu.AddSeparator();
+            FarmMenu.Add("qLaneClear", new CheckBox("Use Q to Lane Clear"));
+            FarmMenu.Add("wLaneClear", new CheckBox("Use W to Lane Clear"));
+            FarmMenu.Add("rLaneClear", new CheckBox("Use R to Lane Clear"));
+            FarmMenu.AddSeparator();
+            FarmMenu.Add("qJG", new CheckBox("Use Q to Jungle Clear"));
+            FarmMenu.Add("wJG", new CheckBox("Use W to Jungle Clear"));
+            FarmMenu.Add("rJG", new CheckBox("Use R to Jungle Clear"));
+            FarmMenu.AddSeparator();
+            FarmMenu.Add("rLastHit", new CheckBox("Use R to Last Hit"));
+
+            MiscMenu = GathMenu.AddSubMenu("Misc", "Misc");
+            MiscMenu.AddGroupLabel("Misc. Settings");
+            MiscMenu.AddSeparator();
+            MiscMenu.Add("interrupt", new CheckBox("Use Q to Interrupt"));
+
+            Game.OnTick += Game_OnTick;
+            Interrupter.OnInterruptableSpell += StateHandler.Interrupter_OnInterruptableSpell;
+
+            EloBuddy.Chat.Print("NerdGath : Thanks for using my script! Enjoy the game!");
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                StateHandler.Combo();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateHandler.Harass();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.JungleClear))
+            {
+                StateHandler.Farm("Jungle");
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                StateHandler.Farm("Lane");
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit))
+            {
+                StateHandler.Farm("LastHit");
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee))
+            {
+                StateHandler.Flee();
+            }
+
+            if (Program.FleeMenu["doKS"].Cast<CheckBox>().CurrentValue)
+            {
+                StateHandler.KS();
+            }
+        }
+    }
+}
---
+++ NerdGath/StateHandler.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using extent = EloBuddy.SDK.Extensions;
+
+namespace NerdGath
+{
+    class StateHandler
+    {
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static Spell.Targeted ignite;
+
+        public static float GetDynamicRange()
+        {
+            if (Program.Q.IsReady())
+            {
+                return Program.Q.Range;
+            }
+            return _Player.GetAutoAttackRange();
+        }
+
+        public static void Interrupter_OnInterruptableSpell(Obj_AI_Base unit, InterruptableSpellEventArgs spell)
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            if (Program.MiscMenu["interrupt"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady())
+            {
+                if (unit.Distance(_Player.ServerPosition, true) <= Program.Q.Range)
+                {
+                    Program.Q.Cast(target);
+                }
+            }
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            var t = TargetSelector2.GetTarget(Program.R.Range, DamageType.Magical);
+            var summonerIgnite = Player.Spells.FirstOrDefault(o => o.SData.Name == "summonerdot"); // Thanks finn
+
+            float rdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, target, EloBuddy.SpellSlot.R); // damage of cho R
+
+            if (target == null) return;
+
+            var manaPre = _Player.ManaPercent > Program.MinNumberManaC;
+
+            if (!manaPre)
+            {
+                return;
+            }
+
+            if (summonerIgnite != null)
+            {
+                SpellSlot igSlot = extent.GetSpellSlotFromName(_Player, "summonerdot");
+                ignite = new Spell.Targeted(igSlot, 600);
+                if (Program.ComboMenu["igniteKill"].Cast<CheckBox>().CurrentValue && ignite.IsReady())
+                {
+                    if (getIgniteDamage() > target.Health - 10) //-10 on enemy health for safecheck
+                    {
+                        ignite.Cast(target);
+                    }
+                }
+            }
+
+            if (Program.ComboMenu["useQCombo"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+            {
+                var qPred = Program.Q.GetPrediction(target);
+                if (qPred.HitChance >= HitChance.High)
+                {
+                    Program.Q.Cast(target);
+                }
+            }
+            if (Program.ComboMenu["useWCombo"].Cast<CheckBox>().CurrentValue && Program.W.IsReady() && target.IsValidTarget(Program.W.Range))
+            {
+                Program.W.Cast(target);
+            }
+            if (Program.ComboMenu["useRCombo"].Cast<CheckBox>().CurrentValue && Program.ComboMenu["useRKill"].Cast<CheckBox>().CurrentValue && Program.R.IsReady() && rdmg > target.Health - 10)
+            {
+                Program.R.Cast(target);
+            }
+            if (Program.ComboMenu["useRCombo"].Cast<CheckBox>().CurrentValue && !Program.ComboMenu["useRKill"].Cast<CheckBox>().CurrentValue && Program.R.IsReady())
+            {
+                Program.R.Cast(target);
+            }
+        }
+
+        public static void Harass()
+        {
+            var manaPre = _Player.ManaPercent > Program.MinNumberManaH;
+            if (!manaPre)
+            {
+                return;
+            }
+
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            if (target == null) return;
+            if (Program.HarassMenu["useQHarass"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+            {
+                var qPred = Program.Q.GetPrediction(target);
+                if (qPred.HitChance >= HitChance.High)
+                {
+                    Program.Q.Cast(target);
+                }
+            }
+
+            if (Program.HarassMenu["useWHarass"].Cast<CheckBox>().CurrentValue && Program.W.IsReady() && target.IsValidTarget(Program.W.Range))
+            {
+                Program.W.Cast(target);
+            }
+        }
+
+        public static void Flee()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            if (Program.FleeMenu["useQFlee"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady())
+            {
+                Program.Q.Cast(target);
+            }
+        }
+
+        public static void KS()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            float qdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, target, EloBuddy.SpellSlot.Q); // damage of cho Q
+            float wdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, target, EloBuddy.SpellSlot.W); // damage of cho W
+            float rdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, target, EloBuddy.SpellSlot.R); // damage of cho R
+            var summonerIgnite = Player.Spells.FirstOrDefault(o => o.SData.Name == "summonerdot"); // Thanks finn
+
+            if (summonerIgnite != null)
+            {
+                SpellSlot igSlot = extent.GetSpellSlotFromName(_Player, "summonerdot");
+                ignite = new Spell.Targeted(igSlot, 600);
+                if (Program.KSMenu["igniteKill"].Cast<CheckBox>().CurrentValue && ignite.IsReady())
+                {
+                    if (getIgniteDamage() > target.Health - 10) //-10 on enemy health for safecheck
+                    {
+                        ignite.Cast(target);
+                    }
+                }
+            }
+
+            if (Program.KSMenu["qKS"].Cast<CheckBox>().CurrentValue && qdmg >= target.Health - 10 && Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+            {
+                var qPred = Program.Q.GetPrediction(target);
+                if (qPred.HitChance >= HitChance.High)
+                {
+                    Program.Q.Cast(target);
+                }
+            }
+            else if (Program.KSMenu["wKS"].Cast<CheckBox>().CurrentValue && wdmg >= target.Health - 10 && Program.W.IsReady() && target.IsValidTarget(Program.W.Range))
+            {
+                Program.W.Cast(target);
+            }
+            else if (Program.KSMenu["rKS"].Cast<CheckBox>().CurrentValue && rdmg >= target.Health - 10 && Program.R.IsReady() && target.IsValidTarget(Program.R.Range))
+            {
+                Program.R.Cast(target);
+            }
+        }
+
+        public static void Farm(String type)
+        {
+            if (type == "Jungle")
+            {
+                foreach (Obj_AI_Base minion in EntityManager.GetJungleMonsters())
+                {
+                    float rdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, minion, EloBuddy.SpellSlot.R); // damage of cho R
+                    if (minion.IsValidTarget(1000f))
+                    {
+                        if (Program.FarmMenu["qJG"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && minion.IsValidTarget(Program.Q.Range))
+                        {
+                            Program.Q.Cast(minion);
+                        }
+                        if (Program.FarmMenu["wJG"].Cast<CheckBox>().CurrentValue && Program.W.IsReady() && minion.IsValidTarget(Program.W.Range))
+                        {
+                            Program.W.Cast(minion);
+                        }
+                        if (Program.FarmMenu["rJG"].Cast<CheckBox>().CurrentValue && Program.R.IsReady() && minion.IsValidTarget(Program.R.Range) && rdmg > minion.Health)
+                        {
+                            Program.R.Cast(minion);
+                        }
+                    }
+                }
+            }
+            else if (type == "Lane")
+            {
+                foreach (Obj_AI_Base minion in EntityManager.GetLaneMinions())
+                {
+                    float rdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, minion, EloBuddy.SpellSlot.R); // damage of cho R
+                    if (minion.IsValidTarget(1000f))
+                    {
+                        if (Program.FarmMenu["qLaneClear"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && minion.IsValidTarget(Program.Q.Range))
+                        {
+                            Program.Q.Cast(minion);
+                        }
+                        if (Program.FarmMenu["wLaneClear"].Cast<CheckBox>().CurrentValue && Program.W.IsReady() && minion.IsValidTarget(Program.W.Range))
+                        {
+                            Program.W.Cast(minion);
+                        }
+                        if (Program.FarmMenu["RLaneClear"].Cast<CheckBox>().CurrentValue && Program.R.IsReady() && minion.IsValidTarget(Program.R.Range) && rdmg > minion.Health)
+                        {
+                            Program.R.Cast(minion);
+                        }
+                    }
+                }
+            }
+            else if (type == "LastHit")
+            {
+                foreach (Obj_AI_Base minion in EntityManager.GetLaneMinions())
+                {
+                    float rdmg = EloBuddy.SDK.DamageLibrary.GetSpellDamage(_Player, minion, EloBuddy.SpellSlot.R); // damage of cho R
+                    if (minion.IsValidTarget(1000f))
+                    {
+                        if (Program.FarmMenu["rLastHit"].Cast<CheckBox>().CurrentValue && Program.R.IsReady() && minion.IsValidTarget(Program.R.Range) && rdmg > minion.Health)
+                        {
+                            Program.R.Cast(minion);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static int getIgniteDamage()
+        {
+            return 50 + 20 * _Player.Level;
+        }
+    }
+}
