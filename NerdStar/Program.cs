5434f92cdf84e782c294ec5f4f418d11e86d67d4
berbb <tintinridge@gmail.com>
2015-09-18 23:30:43 -0500

    NerdStar

---
+++ NerdStar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace NerdStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static Spell.Active Q;
+        public static Spell.Targeted W;
+
+        public static Menu AliMenu, ComboMenu;
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            TargetSelector2.init();
+            Bootstrap.Init(null);
+
+            Q = new Spell.Active(SpellSlot.Q);
+            W = new Spell.Targeted(SpellSlot.W, 630); // reduced the range so it will rarely miss the combo
+
+            AliMenu = MainMenu.AddMenu("NerdStar", "NerdStar");
+            AliMenu.AddGroupLabel("NerdStar");
+            AliMenu.AddSeparator();
+            AliMenu.AddLabel("Nerd Series - Downloading More Ram");
+            AliMenu.AddLabel("Berb @ EloBuddy");
+            AliMenu.AddSeparator();
+            AliMenu.AddLabel("What is this?");
+            AliMenu.AddLabel("This is a very simple Alistar Combo addon. All it does it do the Q>W combo for you!");
+
+            ComboMenu = AliMenu.AddSubMenu("Combo", "Combo");
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("doCombo", new KeyBind("Do W+Q Combo", false, KeyBind.BindTypes.HoldActive, 'a'));
+
+            Game.OnTick += Game_OnTick;
+
+            EloBuddy.Chat.Print("NerdStar : Thanks for using my script! Enjoy the game!");
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo) || Program.ComboMenu["doCombo"].Cast<KeyBind>().CurrentValue)
+            {
+                StateHandler.Combo();
+                Orbwalker.MoveTo(Game.CursorPos);
+            }
+        }
+    }
+}
---
+++ NerdStar/StateHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using extent = EloBuddy.SDK.Extensions;
+
+namespace NerdStar
+{
+    class StateHandler
+    {
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+
+        public static float GetDynamicRange()
+        {
+            if (Program.Q.IsReady())
+            {
+                return Program.Q.Range;
+            }
+            return _Player.GetAutoAttackRange();
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            float getDist = EloBuddy.SDK.Extensions.Distance(_Player, target) / 2.0f; // formula of swag
+            if (target == null) return;
+            if (Program.W.IsReady() && Program.Q.IsReady() && target.IsValidTarget(Program.W.Range))
+            {
+                if (target.IsValidTarget(220)) // if target is close then apply combo fast NO DELAY
+                {
+                    Program.W.Cast(target);
+                    Program.Q.Cast();
+                }
+                else // if target is a bit farther then apply combo with delay
+                {
+                    Program.W.Cast(target);
+                    EloBuddy.SDK.Core.DelayAction(() => { Program.Q.Cast(); }, (int)getDist);
+                }
+            }
+        }
+    }
+}
+
