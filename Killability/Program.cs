804b0846193f87cfa250283a70f48d43c181455e
berbb <tintinridge@gmail.com>
2015-09-19 21:50:27 -0500

    Initial Template of Killability

---
+++ Killability/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Killability
+{
+    class Program
+    {
+
+        public static Menu KillMenu, SettingsM;
+        public static bool alreadyPosted = false;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Bootstrap.Init(null);
+
+            KillMenu = MainMenu.AddMenu("Killability", "Killability");
+            KillMenu.AddGroupLabel("Killability");
+            KillMenu.AddSeparator();
+            KillMenu.AddLabel("Nerd Utility Series - Downloading More Ram");
+            KillMenu.AddLabel("Berb @ EloBuddy");
+            KillMenu.AddSeparator();
+            KillMenu.AddLabel("What is this?");
+            KillMenu.AddLabel("It shows you what skills are needed to kill the enemy.");
+
+            SettingsM = KillMenu.AddSubMenu("Settings", "Settings");
+            SettingsM.AddGroupLabel("Settings");
+            SettingsM.AddSeparator();
+            SettingsM.Add("enabled", new KeyBind("Enable?", false, KeyBind.BindTypes.HoldActive, 'a'));
+
+            Game.OnTick += Game_OnTick;
+
+            EloBuddy.Chat.Print("Killability : Thanks for using my script! Enjoy the game!");
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Program.SettingsM["enabled"].Cast<KeyBind>().CurrentValue)
+            {
+            }
+        }
+    }
+}
