b089c948211857ded102ef470b464cbba9493be2
berbb <tintinridge@gmail.com>
2015-09-19 22:31:00 -0500

    Ahri Dmg Calculations

--- Killability/DamageLib.cs
+++ Killability/DamageLib.cs
@@ -23,7 +23,7 @@ namespace Killability
             var edmg = 0.0f;
             var rdmg = 0.0f;
 
-            if (_Player.ChampionName == "Aatrox") 
+            if (_Player.ChampionName == "Aatrox")
             {
                 qdmg = _Player.CalculateDamageOnUnit(targ, DamageType.Physical, (float)(new[] { 70, 115, 160, 205, 250 }[Program.Q.Level] + 0.6 * _Player.BaseAttackDamage));
                 if (HaveAatroxWDmg)
@@ -33,12 +33,20 @@ namespace Killability
                 edmg = _Player.CalculateDamageOnUnit(targ, DamageType.Mixed, (float)(new[] { 75, 110, 145, 180, 215 }[Program.E.Level] + (0.6 * _Player.BaseAbilityDamage) + (0.6 * _Player.BaseAttackDamage)));
                 rdmg = _Player.CalculateDamageOnUnit(targ, DamageType.Magical, (float)(new[] { 200, 300, 400 }[Program.R.Level] + 1.0 * _Player.TotalMagicalDamage));
             }
+            else if (_Player.ChampionName == "Ahri")
+            {
+                qdmg = _Player.CalculateDamageOnUnit(targ, DamageType.Magical, (float)(new[] { 40, 65, 90, 115, 140 }[Program.Q.Level] + 0.35 * _Player.TotalMagicalDamage));
+                qdmg = qdmg + _Player.CalculateDamageOnUnit(targ, DamageType.True, (float)(new[] { 40, 65, 90, 115, 140 }[Program.Q.Level] + 0.35 * _Player.TotalMagicalDamage));
+                wdmg = _Player.CalculateDamageOnUnit(targ, DamageType.Magical, (float)(new[] { 40, 65, 90, 115, 140 }[Program.W.Level] + 0.4 * _Player.TotalMagicalDamage));
+                edmg = _Player.CalculateDamageOnUnit(targ, DamageType.Magical, (float)(new[] { 60, 90, 120, 150, 200 }[Program.E.Level] + 0.5 * _Player.TotalMagicalDamage));
+                rdmg = _Player.CalculateDamageOnUnit(targ, DamageType.Magical, (float)(new[] { 70, 110, 150 }[Program.R.Level] + 0.3 * _Player.TotalMagicalDamage) * 2); //2 spirit rushes?
+            }
 
-            if (qdmg + wdmg + rdmg > targ.Health) 
+            if (qdmg + wdmg + rdmg > targ.Health)
             {
                 return true;
-            } 
-            else 
+            }
+            else
             {
                 return false;
             }
