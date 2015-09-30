79121938ca9fb7b2ed90ed566e3fba31cadccf40
berbb <tintinridge@gmail.com>
2015-09-30 04:04:53 -0500

    NerdLeona

---
+++ NerdLeona/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace NerdLeona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static Spell.Active Q;
+        public static Spell.Active W;
+        public static Spell.Skillshot E;
+        public static Spell.Skillshot R;
+
+        private static Slider _numR;
+        public static int MinNumberR { get { return _numR.CurrentValue; } }
+
+        private static Slider manaC;
+        public static int MinNumberManaC { get { return manaC.CurrentValue; } }
+
+        private static Slider manaH;
+        public static int MinNumberManaH { get { return manaH.CurrentValue; } }
+
+        public static Menu LeoMenu, ComboMenu, HarassMenu, FleeMenu, MiscMenu;
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            TargetSelector2.init();
+            Bootstrap.Init(null);
+
+            Q = new Spell.Active(SpellSlot.Q);
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Skillshot(SpellSlot.E, 875, SkillShotType.Linear);
+            R = new Spell.Skillshot(SpellSlot.R, 1200, SkillShotType.Circular);
+
+            LeoMenu = MainMenu.AddMenu("NerdLeona", "NerdLeona");
+            LeoMenu.AddGroupLabel("NerdLeona");
+            LeoMenu.AddSeparator();
+            LeoMenu.AddLabel("Nerd Series - Downloading More Ram");
+            LeoMenu.AddLabel("Berb @ EloBuddy");
+            LeoMenu.AddSeparator();
+            LeoMenu.AddLabel("TO-DO ::");
+
+            ComboMenu = LeoMenu.AddSubMenu("Combo", "Combo");
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("useQCombo", new CheckBox("Use Q"));
+            ComboMenu.Add("useWCombo", new CheckBox("Use W"));
+            ComboMenu.Add("useECombo", new CheckBox("Use E"));
+            ComboMenu.Add("useRCombo", new CheckBox("Use R"));
+            ComboMenu.Add("igniteAlways", new CheckBox("Always use ignite"));
+            ComboMenu.Add("igniteKill", new CheckBox("Ignite if Killable"));
+            ComboMenu.AddSeparator();
+            _numR = ComboMenu.Add("useRinEnemy", new Slider("Use R if enemy in range", 2, 1, 5));
+            ComboMenu.AddSeparator();
+            manaC = ComboMenu.Add("manamanager", new Slider("Minimum mana to do combo (%)", 20, 1, 100));
+
+            HarassMenu = LeoMenu.AddSubMenu("Harass", "Harass");
+            HarassMenu.AddGroupLabel("Harass Settings");
+            HarassMenu.AddSeparator();
+            HarassMenu.Add("useQHarass", new CheckBox("Use Q"));
+            HarassMenu.Add("useEHarass", new CheckBox("Use E"));
+            HarassMenu.AddSeparator();
+            manaH = HarassMenu.Add("manamanager", new Slider("Minimum mana to harass (%)", 20, 1, 100));
+
+            MiscMenu = LeoMenu.AddSubMenu("Misc", "Misc");
+            MiscMenu.AddGroupLabel("Misc. Settings");
+            MiscMenu.AddSeparator();
+            MiscMenu.Add("interrupt", new CheckBox("Use Spells to Interrupt"));
+
+            Game.OnTick += Game_OnTick;
+            Interrupter.OnInterruptableSpell += StateHandler.Interrupter_OnInterruptableSpell;
+
+            EloBuddy.Chat.Print("NerdLeona : Thanks for using my script! Enjoy the game!");
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                StateHandler.Combo();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateHandler.Harass();
+            }
+        }
+    }
+}
---
+++ NerdLeona/StateHandler.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using SharpDX;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using extent = EloBuddy.SDK.Extensions;
+
+namespace NerdLeona
+{
+    class StateHandler
+    {
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static Spell.Targeted ignite;
+        static Vector3 mousePos { get { return Game.CursorPos; } }
+
+        public static float GetDynamicRange()
+        {
+            if (Program.Q.IsReady())
+            {
+                return Program.Q.Range;
+            }
+            return _Player.GetAutoAttackRange();
+        }
+
+        public static void Interrupter_OnInterruptableSpell(Obj_AI_Base unit, Interrupter.InterruptableSpellEventArgs spell)
+        {
+            if (Program.MiscMenu["interrupt"].Cast<CheckBox>().CurrentValue && Program.E.IsReady())
+            {
+                if (unit.Distance(_Player.ServerPosition, true) <= Program.E.Range)
+                {
+                    Program.E.Cast(unit);
+                }
+            }
+            if (Program.MiscMenu["interrupt"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady())
+            {
+                if (unit.Distance(_Player.ServerPosition, true) <= Program.Q.Range)
+                {
+                    Program.Q.Cast(unit);
+                }
+            }
+            if (Program.MiscMenu["interrupt"].Cast<CheckBox>().CurrentValue && Program.R.IsReady())
+            {
+                if (unit.Distance(_Player.ServerPosition, true) <= Program.R.Range)
+                {
+                    Program.R.Cast(unit);
+                }
+            }
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            var t = TargetSelector2.GetTarget(Program.R.Range, DamageType.Magical);
+            var summonerIgnite = Player.Spells.FirstOrDefault(o => o.SData.Name == "summonerdot"); // Thanks finn
+
+            if (target == null) return;
+
+            var manaPre = _Player.ManaPercent > Program.MinNumberManaC;
+
+            if (!manaPre)
+            {
+                return;
+            }
+
+            if (summonerIgnite != null)
+            {
+                SpellSlot igSlot = extent.GetSpellSlotFromName(_Player, "summonerdot");
+                ignite = new Spell.Targeted(igSlot, 600);
+                if (Program.ComboMenu["igniteKill"].Cast<CheckBox>().CurrentValue && ignite.IsReady())
+                {
+                    if (getIgniteDamage() >= target.Health - 5) //-5 on enemy health for safecheck & HP regen
+                    {
+                        ignite.Cast(target);
+                    }
+                }
+                else if (Program.ComboMenu["igniteAlways"].Cast<CheckBox>().CurrentValue && ignite.IsReady())
+                {
+                    ignite.Cast(target);
+                }
+            }
+
+            if (Program.ComboMenu["useWCombo"].Cast<CheckBox>().CurrentValue && Program.W.IsReady())
+            {
+                Program.W.Cast();
+            }
+            if (Program.ComboMenu["useECombo"].Cast<CheckBox>().CurrentValue && Program.E.IsReady() && target.IsValidTarget(Program.E.Range))
+            {
+                Program.E.Cast(target);
+            }
+            if (Program.ComboMenu["useQCombo"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady())
+            {
+                Program.Q.Cast();
+            }
+            if (Program.ComboMenu["useRCombo"].Cast<CheckBox>().CurrentValue && Program.R.IsReady() && target.IsValidTarget(Program.R.Range) && t.CountEnemiesInRange(Program.R.Range) >= Program.MinNumberR)
+            {
+                Program.R.Cast(t);
+            }
+        }
+
+        public static void Harass()
+        {
+            var manaPre = _Player.ManaPercent > Program.MinNumberManaH;
+            if (!manaPre)
+            {
+                return;
+            }
+
+            var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
+            if (target == null) return;
+            if (Program.HarassMenu["useQHarass"].Cast<CheckBox>().CurrentValue && Program.Q.IsReady() && target.IsValidTarget(Program.Q.Range))
+            {
+                Program.Q.Cast(target);
+            }
+            if (Program.HarassMenu["useEHarass"].Cast<CheckBox>().CurrentValue && Program.E.IsReady() && target.IsValidTarget(Program.E.Range))
+            {
+                Program.E.Cast(target);
+            }
+        }
+
+        public static int getIgniteDamage()
+        {
+            return 50 + 20 * _Player.Level;
+        }
+    }
+}
